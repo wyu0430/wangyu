3717b1e28d9c9c73a3cdd850d907b7781712606d
wyu0430 <wyu0430@163.com>
2018-11-29 16:45:12 +0800

    update 20181129

--- 多线程基础练习/test_cli.py
+++ 多线程基础练习/test_cli.py
@@ -1,14 +1,53 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
-url = ('127.0.0.1', 20000)
-ss = socket.socket()
-ss.connect(url)
+#socket客户端
 
-while True:
-    message = input("input>")
-    if not message:
-        break
-    ss.send(message.encode('utf8'))
-    rec_message = ss.recv(1024)
-    print(rec_message.decode('utf8'))
-ss.close()
+class Cli:
+    def __init__(self,ip = '127.0.0.1',port = 20002):
+        """
+        初始化客户端,设置链接ip，port,建立线程池
+        """
+        self.ss = socket.socket()
+        self.connect_ip = (ip,port)
+        self.pool = ThreadPoolExecutor(5)
+
+    def conn(self):
+        """
+        建立链接
+        :return:
+        """
+        self.ss.connect(self.connect_ip)
+
+    def cli_send(self):
+        """
+        发送消息
+        :return:
+        """
+        while True:
+            message = input('input>>>')
+            self.ss.send(message.encode())
+
+    def cli_receive(self):
+        """
+        接收消息
+        :return:
+        """
+        while True:
+            msg = self.ss.recv(1024)
+            print(msg.decode())
+
+    def start(self):
+        """
+        开始客户端，把接收和发送放入到线程中
+        :return:
+        """
+        print('start cli')
+        self.conn()
+        self.pool.submit(self.cli_send)
+        self.pool.submit(self.cli_receive)
+
+
+if __name__=='__main__':
+    cli = Cli()
+    cli.start()
--- 多线程基础练习/test_server.py
+++ 多线程基础练习/test_server.py
@@ -1,30 +1,63 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
-from threading import Thread
+#socket 服务端
 
-def message(cli,address):
-
-
-    print('接受来自{}的消息'.format(address))
-    while True:
-        cli_message = cli.recv(1024)
-        if not cli_message:
-            break
-        rec_mesage = '收到的消息{}'.format(cli_message.decode('utf8'))
-        print(rec_mesage)
-        cli.send(rec_mesage.encode('utf8'))
-
-
-
+class Server:
+    def __init__(self,ip = '127.0.0.1',port = 20002):
+        """
+        初始化服务端
+        """
+        self.bind_addr = (ip,port)
+        self.ss = socket.socket()
+        self.pools = ThreadPoolExecutor(10)
+        self.ss.bind(self.bind_addr)
+        self.ss.listen(5)
+        self.conn_list = []
 
 
+    def send_message(self,conn,msg,address):
+        """
 
-ss = socket.socket()
-url = ('127.0.0.1',20000)
-ss.bind(url)
-ss.listen(2)
-while True:
-    cli,address = ss.accept()
-    Thread(target=message,args=(cli,address)).start()
+        :return:
+        """
+
+        conn.send('{}说{}'.format(address,msg.decode()).encode())
+
+    def receive_message(self,conn,address):
+        """
+
+        :return:
+        """
+        while True:
+            msg = conn.recv(1024)
+            print('接收到ip{}的消息{}'.format(address,msg.decode()))
+            self.send_all_message(msg,address)
+
+    def server_acc(self):
+        """
+        建立链接
+        :return:
+        """
+        while True:
+            conn,address = self.ss.accept()
+            self.conn_list.append(conn)
+            self.pools.submit(self.receive_message,conn,address)
 
 
+    def send_all_message(self,msg,address):
+        """
+        给所有链接客户端发送消息
+        :return:
+        """
+        for conn in self.conn_list:
+            self.send_message(conn,msg,address)
+
+    def start(self):
+        print('start server')
+        self.server_acc()
+
+
+if __name__=="__main__":
+    server = Server()
+    server.start()
